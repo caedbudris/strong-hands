1418f8160c013a46d590041e6f8fb70b4429ccee
caedbudris <caed.budris@gmail.com>
2015-12-12 20:48:16 +0000

    adding payments with stripe and styling

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,18 +4,32 @@ class ChargesController < ApplicationController
     end
 
     def create
-        @amount = params[:amount] * 10 * 100
+        @quantity = order_params[:amount].to_i * 10
         
-        token = params[:stripeToken]
+        @shipping = order_params[:shipping]
+        if @shipping == "United States"
+            @quantity = @quantity + 3
+        elsif @shipping == "Canada"
+            @quantity = @quantity + 10
+        end
+        
+        amount =  @quantity * 100
+            
+        token = order_params[:stripeToken]
         
         charge = Stripe::Charge.create(
-            amount: @amount,
-            currency: 'usd',
-            source: token,
-            description: 'Barbell Balm'
+            :amount => amount,
+            :currency => 'usd',
+            :source => token,
+            :description => 'Barbell Balm'
         )
         
         rescue Stripe::CardError => e
     end
     
+    protected
+        
+        def order_params
+            params.permit(:stripeToken, :amount, :shipping) 
+        end
 end
